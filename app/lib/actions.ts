21b2345b6e8b51baeb79c2e283a86e7f06c6c1b3
jinggo <wangyj0898@126.com>
2024-05-03 16:14:17 +0800

    chapter12-3. Extract the data from formData

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,4 +2,12 @@
 
 export async function createInvoice(formData: FormData) {
 
+    // const rawFormData = Object.fromEntries(formData.entries())
+    const rawFormData = {
+        amount: formData.get('amount'),
+        customerId: formData.get('customerId'),
+        status: formData.get('status')
+    };
+
+    console.log(rawFormData)
 }
