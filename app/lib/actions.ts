8c03c0f7feaf8e03241af732df27112aab22be5f
jinggo <wangyj0898@126.com>
2024-05-04 09:56:25 +0800

    chapter12-updating an invoice(4. Pass the id to the Server Action)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,12 +13,15 @@ const FormSchema = z.object({
     date: z.string(),
 });
 
-const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const CreateInvoice = FormSchema.omit({id: true, date: true});
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({id: true, date: true});
 
 export async function createInvoice(formData: FormData) {
 
     // const rawFormData = Object.fromEntries(formData.entries())
-    const { customerId, amount, status } = CreateInvoice.parse({
+    const {customerId, amount, status} = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
@@ -30,6 +33,25 @@ export async function createInvoice(formData: FormData) {
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const {customerId, amount, status} = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
   `;
 
     revalidatePath('/dashboard/invoices');
